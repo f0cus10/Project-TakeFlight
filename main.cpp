467cbbf78eface1ebc1d56d952184e6293e937fd
Iftikhar Khan <iakhan4425@gmail.com>
2018-02-19 14:15:14 -0500

    Added response

--- main.cpp
+++ main.cpp
@@ -1,14 +1,57 @@
+//
+//  main.cpp
+//  takeflight
+//
+//  Created by Iftikhar Khan on 2/19/18.
+//  Copyright © 2018 Iftikhar Khan. All rights reserved.
+//
 
-/* Headers for making a request */
+#include <iostream> //To print to stdin
 
+/* To get current time data */
+#include <ctime>
+#include <chrono>
+
+/* To make http requests using REST API */
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
 
-using namespace utility;                    // Common utilities like string conversions
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace web::http::client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
 
-int main(){
+using namespace utility;
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+using namespace concurrency::streams;
+using std::cout;
+using std::endl;
+
+
+int main(int argc, const char * argv[]) {
+  auto filestream = std::make_shared<ostream>();
+  pplx::task<void> requestTask = fstream::open_ostream(U("result.json")).then([=] (ostream outFile){
+    *filestream = outFile;
+    http_client client(U("https://opensky-network.org"));
+    
+    //Build the uri builder and start the request
+    uri_builder builder(U("/api/states/all"));
+    //TODO (BuildError #1): Need to convert std::chrono::time_t to string to allow making a request
+    builder.append_query(U("time"), U(std::chrono::system_clock::now()));
+    return client.request(methods::GET, builder.to_string());
+  })
+  .then([=] (http_response response){
+    cout << "Received response code: " << response.status_code() << endl;
+    return response.body().read_to_end(filestream->streambuf());
+  })
+  .then([=] (size_t){
+    return filestream->close();
+  });
+  
+  try{
+    requestTask.wait();
+  }
+  catch(const std::exception &e){
+    printf("Error Exception:%s\n", e.what());
+  }
+  return 0;
 }
+
